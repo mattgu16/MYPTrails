0316b225a77fde766ccb8142ea3d8b7030dbd1b7
pithegreat <pithegreat@users.noreply.github.com>
2016-04-08 09:52:28 -0400

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,2 +1,25 @@
-Modify file as needed
-Test 1
+#Finds trail info in text document and orginizes it
+def read_trails(path):
+	file = open(path)
+	text = file.read()
+	trails = []
+	g = text.find('>')
+	while g != -1:
+		dictionary = {}
+		name = text[g+1:text.find('\n',g)]
+		dictionary['Name'] = name
+		g = text.find('Where:',g+1)
+		where = text[g+7: text.find('\n', g+1)]
+		dictionary['Where'] = where
+		g = text.find('Length:',g+1)
+		length = text[g+8: text.find('\n', g+1)]
+		dictionary['Length'] = length
+		g = text.find('Difficulty:',g+1)
+		difficulty = text[g+12: text.find('\n', g+1)]
+		dictionary['Difficulty'] = difficulty
+		g = text.find('Notes:',g+1)
+		notes = text[g+6:text.find('>',g+1)]
+		dictionary['Notes'] = notes
+		trails.append(dictionary)
+		g = text.find('>',g+1)
+	print(trails)
