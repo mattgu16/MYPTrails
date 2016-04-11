0b0fd9730af1c028a6cfee9fad038a91d4c962ea
pithegreat <pithegreat@users.noreply.github.com>
2016-04-11 09:35:37 -0400

    Update file1.py

--- file1.py
+++ file1.py
@@ -1,4 +1,4 @@
-#Finds trail info in text document and orginizes it
+#Finds trail info in text document and orginizes it into a list with dictionaries of the information for each trail
 def read_trails(path):
 	file = open(path)
 	text = file.read()
@@ -18,8 +18,17 @@ def read_trails(path):
 		difficulty = text[g+12: text.find('\n', g+1)]
 		dictionary['Difficulty'] = difficulty
 		g = text.find('Notes:',g+1)
-		notes = text[g+6:text.find('>',g+1)]
+		notes = text[g+6:text.find('Source:',g+1)]
 		dictionary['Notes'] = notes
+		g = text.find('Source:', g+1)
+		comma = text.find(',', g+1)
+		newline = text.find('\n',g)
+		source = ''
+		if newline >= comma:
+			source = [text[g+8: comma], text[comma+2: newline]]
+		if newline <= comma:
+			source = [text[g+8: text.find('\n', g+1)]]
+		dictionary['Sources'] = source
 		trails.append(dictionary)
 		g = text.find('>',g+1)
-	print(trails)
+	return trails
