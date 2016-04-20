989b19eee71483190d6fa8bb9e253ffac6bafb39
pithegreat <pithegreat@users.noreply.github.com>
2016-04-20 09:40:15 -0400

    Added files via upload

---
+++ Trails-code/README.md
@@ -0,0 +1,17 @@
+Trails ![Build Status] (https://travis-ci.org/mattgu16/Trails.svg?branch=master)
+============
+#####Maryland Hiking Trail Filter#####
+
+This Python project allows you to search through (most) hiking trails in the state of Maryland through a variety of attributes.
+
+###Usage
+_Prerequisites: This project requires you to have Python installed on your computer. Download at [http://python.org](https://www.python.org/downloads/)._
+
+1. [Download the latest release](https://github.com/mattgu16/Trails/releases/).
+2. Unzip the folder with third party software (or Windows default extracting). _[Not extracting may cause errors in the unicode]_
+3. **Remember to keep all of the files in the same folder.**
+4. Run main.py
+
+###Notes
+- This project _will_ be able to open your default browser to display filtered results. Please make sure your default browser is ***not*** Internet Explorer.
+- This version of the project is in beta, and thus is unstable. By the way, there is no current user interface so yeah.  We will keep you posted.
---
+++ Trails-code/main.py
@@ -0,0 +1,171 @@
+import webbrowser
+import os
+#Finds trail info in text document and orginizes it into a list with dictionaries of the information for each trail
+def main():
+        print("Would you like to search by length, by location or by difficulty?")
+        print("")
+        a=input("L for Length,  O for location, D for difficulty: ")
+        print("")
+        validfilter=["L","O","D"]
+        while a not in validfilter:
+                a=input("L for Length,  O for location, D for difficulty: ")
+                print("")
+        if a.upper()=="L":
+                find_length()
+        elif a.upper()=="O":
+                f=input("Choose one: Northern Maryland, Central Maryland, Baltimore, Southern Maryland, Western Maryland, Eastern Shore:  ")
+                find_prop("Where",f)
+        else:
+                p=input("Choose one: Easy, Moderate, Difficult:  ")
+                find_prop("Difficulty",p)
+def read_trails(path):
+        file = open(path, encoding="utf8")
+        text = file.read()
+        trails = []
+        g = text.find('>')
+        while g != -1:
+                dictionary = {}
+                name = text[g+1:text.find('\n',g)]
+                dictionary['Name'] = name
+                g = text.find('Where:',g+1)
+                where = text[g+7: text.find('\n', g+1)]
+                dictionary['Where'] = where
+                g = text.find('Length:',g+1)
+                length = text[g+8: text.find('\n', g+1)]
+                dictionary['Length'] = length
+                g = text.find('Difficulty:',g+1)
+                difficulty = text[g+12: text.find('\n', g+1)]
+                dictionary['Difficulty'] = difficulty
+                g = text.find('Notes:',g+1)
+                notes = text[g+6:text.find('Source:',g+1)]
+                dictionary['Notes'] = notes
+                g = text.find('Source:', g+1)
+                comma = text.find(',', g+1)
+                newline = text.find('\n',g)
+                source = ''
+                if newline >= comma:
+                        source = [text[g+8: comma], text[comma+2: newline]]
+                if newline <= comma:
+                        source = [text[g+8: text.find('\n', g+1)]]
+                dictionary['Sources'] = source
+                trails.append(dictionary)
+                g = text.find('>',g+1)
+        return trails
+    #------------------------------------------------#
+#Adds results in webpage
+def add_results(result_list):
+        file = open("Trails-webpage\index.html", "w")
+        trails = read_trails("trails.txt")
+        file.write('''<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="style.css" type="text/css" />
+        <link rel='shortcut icon' href='images/favicon.ico' type='image/x-icon'/ > 
+        <link href='http://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+        <title>Trail Info</title>
+    </head>
+    <!-------------------------Begin Body------------------------>
+    <body>
+        <header>
+            <br>
+            <br>
+        <a href="#"><div class="content-header-wide">
+            <h1>Trail Information</h1>
+            </div></a>
+            </header>
+        <br>
+        <br>
+        <br>''')
+        for i in result_list:
+                for j in trails:
+                        if j["Name"] == i:
+                                dicty = j
+                file.write('''<div class="content">
+        <table>
+            <tr>''')
+                file.write('''<td><b>Trail Name</b></td>
+''')
+                name = dicty['Name']
+                name_of_trail = '<td>'+name+'</td>'
+                file.write(name_of_trail)
+                file.write('''</tr>
+            <tr>
+            <td><b>Trail Length in miles</b></td>
+            ''')
+                length = '<td>'+dicty['Length']+'</td>'
+                file.write(length)
+                file.write('''
+            </tr>
+            <tr>
+            ''')
+                file.write('''<td><b>Trail Difficulty</b></td>''')
+                difficulty = '<td>'+dicty['Difficulty']+'<td>'
+                file.write(difficulty)
+                file.write('''
+            </tr>
+            <tr>
+            <td><b>About the Trail</b></td>
+            ''')
+                file.write('<td>'+dicty['Notes']+'</td></tr>')
+                file.write('''<tr>
+<td><b>External Links</b></td>''')
+                sources = ''
+                file.write('<td>')
+                file.write('''<div id="navcontainer"><ul>''')
+                for e in dicty['Sources']:
+                        href = '<li>'+'<a href=' + e + '>' + e + '</a>'+'</li>'+'<br>'
+                        file.write(href)
+                file.write('</ul></div></td>')
+                file.write('''</tr>''')
+                file.write(''' </table>
+        </div><br><br><br>''')
+        file.write('''
+   </body>
+</html>''')
+        file.close()
+        #---------------------------------------#
+        #Find trails that meet certain properties
+def find_prop(attr, prop):
+    trails = read_trails('trails.txt')
+    global filtered
+    filtered = []
+    for i in trails:
+        if i[attr] == prop:
+            filtered.append(i["Name"])
+    add_results(filtered)                 
+def find_length():
+    trails = read_trails("trails.txt")
+    mirange=input("Input a mile range: <5, 5-10, 10-15, 15-20, 20+ =>")
+    validrange=["<5","5-10","10-15","15-20","20+"]
+    global filtered
+    filtered=[]
+    while mirange not in validrange:
+        mirange=input("Input a mile range: <5, 5-10, 10-15, 15-20, 20+ =>")
+    for q in trails:
+        length=float(q["Length"])
+        if mirange=="<5":
+            if length<5:
+                filtered.append(q["Name"])
+        elif mirange=="5-10":
+            if length>=5:
+                if length<=10:
+                    filtered.append(q["Name"])
+        elif mirange=="10-15":
+            if length>=10:
+                if length<=15:
+                    filtered.append(q["Name"])
+        elif mirange=="15-20":
+            if length>=15:
+                if length<=20:
+                    filtered.append(q["Name"])
+
+        else:
+            if length>20:
+                filtered.append(q["Name"])
+        add_results(filtered)    
+#---------------Running Program----------------#
+main()
+#--------------Opens Webpage-----------------#
+webbrowser.open('file://' + os.path.realpath('Trails-webpage\index.html'))
