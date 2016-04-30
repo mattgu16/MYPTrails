2d480a3c5c0eccee2a3bcdf392129bdaca04ed16
pithegreat <pithegreat@users.noreply.github.com>
2016-04-30 15:47:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,12 +25,21 @@ def main():
         if a.upper()=="L":
                 find_length()
         elif a.upper()=="O":
-                f=input("Choose one: 'Northern Maryland', 'Central Maryland', 'Baltimore', 'Southern Maryland', 'Western Maryland', or 'Eastern Shore':  ").upper()
+                f=input("Choose one: 'Northern Maryland', 'Central Maryland', 'Baltimore', 'Southern Maryland', 'Western Maryland', or 'Eastern Shore':  ")
+                if (' ' in f):
+                        space = f.split()
+                        f = ''
+                        for i in space:
+                                f = f + i[0].upper() + i[1:].lower() + ' '
+                        f = f[:-1]
+                else:
+                        f = f[0].upper() + f[1:-1].lower()
                 find_prop("Where",f)
         elif a.upper()=="A":
                 find_all()
         else:
-                p=input("Choose one: 'Easy', 'Moderate', 'Difficult':  ").upper()
+                p=input("Choose one: 'Easy', 'Moderate', 'Difficult':  ")
+                p = p[0].upper() + p[1:].lower()
                 find_prop("Difficulty",p)
         openpage()
 #Finds trail info in text document and orginizes it into a list with dictionaries of the information for each trail
